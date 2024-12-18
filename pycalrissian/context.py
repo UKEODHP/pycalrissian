72494e556628cad9d2086850b05cca44939b0c6b
tjellicoe <thomas.jellicoe@telespazio.com>
2024-12-18 14:26:49 +0000

    Add second aws-credentials volume for user services

--- pycalrissian/context.py
+++ pycalrissian/context.py
@@ -62,7 +62,8 @@ class CalrissianContext:
         self.calrissian_wdir = "calrissian-wdir"
 
         # Configure AWS Creds Volume
-        self.aws_credentials_volume_name = f"aws-credentials-{job_id}"
+        self.aws_credentials_workspace_volume_name = f"aws-credentials-workspace-{job_id}"
+        self.aws_credentials_user_service_volume_name = f"aws-credentials-service-{job_id}"
         self.aws_storage_class = "file-storage"
 
         self.labels = labels
@@ -188,13 +189,23 @@ class CalrissianContext:
 
                 assert isinstance(response, V1PersistentVolumeClaim)
 
-        # Create AWS Creds PVC
+        # Create AWS Creds PVCs
         logger.info(
-            f"create persistent volume claim {self.aws_credentials_volume_name} of {self.volume_size} "
+            f"create persistent volume claim {self.aws_credentials_workspace_volume_name} of {self.volume_size} "
             f"with storage class {self.aws_storage_class}"
         )
         response = self.create_pvc(
-            name=self.aws_credentials_volume_name,
+            name=self.aws_credentials_workspace_volume_name,
+            size=self.volume_size,
+            storage_class=self.aws_storage_class,
+            access_modes=["ReadWriteOnce"],
+        )
+        logger.info(
+            f"create persistent volume claim {self.aws_credentials_user_service_volume_name} of {self.volume_size} "
+            f"with storage class {self.aws_storage_class}"
+        )
+        response = self.create_pvc(
+            name=self.aws_credentials_user_service_volume_name,
             size=self.volume_size,
             storage_class=self.aws_storage_class,
             access_modes=["ReadWriteOnce"],
